580943184e6d46a63aa0124f93a6ac90e498e40b
Kamran Azeem <kaz@praqma.net>
2019-11-16 22:20:40 +0100

    fixed a typo

--- website/index.php
+++ website/index.php
@@ -1,7 +1,7 @@
 <?php 
 echo '<h1>Simple website - This is index.php! </h1>'; 
 echo "<hr>";
-echo "<a href=index.php>index.html</a>" . PHP_EOL . "<br>";
+echo "<a href=index.html>index.html</a>" . PHP_EOL . "<br>";
 echo "<a href=phpinfo.php>phpinfo.php</a>" . PHP_EOL . "<br>";
 echo "<a href=createfile.php>createfile.php</a>"  . PHP_EOL . "<br>";
 echo "<a href=dbconnection.php>dbconnection.php</a>"  . PHP_EOL . "<br>";
